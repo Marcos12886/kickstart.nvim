c4a47108d96f0da0eada6500ed78e20f734b8dbb
Marcos12886 <marcos.cenzual8@gmail.com>
2024-12-13 23:48:33 +0100

    Set recommendation from Avante into init

--- init.lua
+++ init.lua
@@ -28,6 +28,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Recomendation from Avante
+vim.opt.laststatus = 3
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
